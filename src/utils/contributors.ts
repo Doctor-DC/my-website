deb9dbcbc201956f9cf7e98447ec6b5075407ff7
Doctor-DC <892062236@qq.com>
2022-08-15 17:41:09 +0800

    token

--- src/utils/contributors.ts
+++ src/utils/contributors.ts
@@ -1,6 +1,6 @@
 import { Octokit } from '@octokit/rest'
 
-const octokit = new Octokit({ auth: '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' });
+const octokit = new Octokit({ auth: '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' });
 
 export async function GetContributors() {
     const {data} = await octokit.repos.listContributors({
