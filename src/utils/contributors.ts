35d376995dd2fa1839acc9863c753ee11be814d3
Doctor-DC <892062236@qq.com>
2022-08-15 17:21:38 +0800

    token

--- src/utils/contributors.ts
+++ src/utils/contributors.ts
@@ -1,6 +1,6 @@
 import { Octokit } from '@octokit/rest'
 
-const octokit = new Octokit({ auth: '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' });
+const octokit = new Octokit({ auth: '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' });
 
 export async function GetContributors() {
     const {data} = await octokit.repos.listContributors({
