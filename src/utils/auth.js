f003467e509b26bd5714a6556f788fb78bf9c6cb
Doctor-DC <892062236@qq.com>
2023-11-24 16:26:47 +0800

    update

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -17,7 +17,8 @@ export async function signInWithGithub() {
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'github',
         options: {
-            redirectTo: process.env.NODE_ENV === 'production' ? 'https://website-dc.vercel.app/' : 'http://localhost:3000'
+            redirectTo: 'https://website-dc.vercel.app/'
+            // redirectTo: 'http://localhost:3000'
         }
       })
 
